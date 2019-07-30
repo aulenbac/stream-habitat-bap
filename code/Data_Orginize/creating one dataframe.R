091f5b74a010352c03480823dbe9a82185d90731
Rebecca <rscully@usgs.gov>
2019-07-30 13:25:38 -0700

    Add data, updates to BLM.csv and code for creating one dataframe

--- code/Data_Orginize/creating one dataframe.R
+++ code/Data_Orginize/creating one dataframe.R
@@ -9,7 +9,7 @@ metadata<-as_tibble(read.xlsx2("Data/Metadata.xlsx", 2, header=TRUE))
 SN <-select(metadata, c(ShortName,AREMPColumn., BLMColumn, EPAColumn, PIBOColumn))
 SN <- as_tibble(lapply(SN, as.character))
 
-#Covnert blaks to missing values 
+#Covnert blankes to missing values 
 SN<-mutate_all(SN, funs(na_if(.,"")))
 
 # Return a sub set of metrics that all 3 programs calculate 
@@ -20,7 +20,6 @@ subSN<-SN %>%
 #Create a variable holding the short names 
 CN= subSN$ShortName
 
-
 #Read in data from PIBO 
 PIBO <- read.xlsx("Data/PIBO_Summ_2013.xlsx",2, header=TRUE)
 PIBO <-as_tibble(PIBO)
@@ -28,11 +27,16 @@ PIBO <-as_tibble(PIBO)
 PIBO_names<- subSN$PIBOColumn
 PIBO_sub<-PIBO %>%
       select(PIBO_names)
+
 #Rename to the standard columen names 
 colnames(PIBO_sub)<-CN
 
 #add a columen to id Program 
-PIBO_sub$Program="PIBO"
+PIBO_sub$Program<-"PIBO"
+
+#change data type of the ReachID and SiteID 
+PIBO_sub$ReachID <- as.character(PIBO_sub$ReachID)
+PIBO_sub$SiteID <- as.character(PIBO_sub$SiteID)
 
 #Load the file from the Data file 
 BLM <- read.csv("Data/BLM.csv")
@@ -47,11 +51,14 @@ BLM_sub <- BLM %>%
 colnames(BLM_sub)<-CN
 
 #add a columen to id Program 
-BLM_sub$Program="BLM"
+BLM_sub$Program<-"BLM"
+
+BLM_sub$ReachID <- as.character(BLM_sub$ReachID)
+BLM_sub$SiteID <- as.character(BLM_sub$SiteID)
 
 # Create one dataset 
-data <- rbind(BLM_sub, PIBO_sub)
+data <- as_tibble(rbind(BLM_sub, PIBO_sub))
 
 #Write data to a .csv
-write.csv(date, file="Data/All_Data.csv")
+write.csv(data, file="Data/All_Data.csv", row.names=FALSE)
 
